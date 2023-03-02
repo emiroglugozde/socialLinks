ee4b9084e39e29595b5207f7353c50acf7de71f3
emiroglugozde <126806398+emiroglugozde@users.noreply.github.com>
2023-03-02 20:32:26 +0300

    Add files via upload

---
+++ title.js
@@ -0,0 +1,20 @@
+var i = 0;
+var titleText = ['g','go','goz','gozd','gozde','gozd','goz','go',];
+
+if (document.addEventListener) {
+	document.addEventListener("DOMContentLoaded", function() {
+		loaded();
+	});
+} else if (document.attachEvent) {
+	document.attachEvent("onreadystatechange", function() {
+		loaded();
+	});
+}
+
+function loaded() {
+	setInterval(loop, 300);
+}
+
+function loop() {
+	document.getElementsByTagName("title")[0].innerHTML = titleText[i++ % titleText.length];
+}
